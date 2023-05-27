5aa3a85b9340e228ff208bdb28ba6a1e0651db0f
lukemccrae <lukemccrae@gmail.com>
2023-05-27 00:54:35 -0600

    sockets not working yet

--- socket-frontend/src/components/grid/Grid.tsx
+++ socket-frontend/src/components/grid/Grid.tsx
@@ -1,25 +1,63 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './grid.css'; 
 
 export const Grid: React.FC = () => {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
   const [buttonStates, setButtonStates] = useState<boolean[]>(Array(64).fill(true));
+
+  useEffect(() => {
+    // Create new WebSocket connection when component mounts
+    const newSocket = new WebSocket("wss://blooming-shore-86317.herokuapp.com:443");
+
+    newSocket.onopen = () => {
+      // console.log("WebSocket connection opened!");
+      if(socket === undefined) { // Check if socket state is undefined
+        setSocket(newSocket);
+      }
+      console.log(socket, '<< socket')
+    };
+
+    newSocket.onclose = () => {
+      console.log("WebSocket connection closed!");
+      setSocket(null);
+    };
+
+    newSocket.onerror = (error) => {
+      console.error("WebSocket error: " + error);
+    };
+
+    // Clean up WebSocket connection when component unmounts
+    return () => {
+      if (socket && socket.readyState === WebSocket.OPEN) {
+        socket.close();
+      }
+    };
+  }, [socket]); // Only run this effect when the socket state changes
 
   const handleButtonClick = (index: number) => {
     console.log(index, '<< index')
     const updatedButtonStates = [...buttonStates];
     updatedButtonStates[index] = !updatedButtonStates[index];
     setButtonStates(updatedButtonStates);
+
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      console.log('im here')
+    // Send a string message to the WebSocket server
+    socket.send("Hello, WebSocket server!");
+  } else {
+    console.error("WebSocket is not connected.");
+  }
   };
 
   return (
     <div className="push2-grid-container">
         <div className="push2-grid">
         {buttonStates.map((state, index) => (
-            <button
-            key={index}
-            className={`push2-button ${state ? 'on' : 'off'}`}
-            onClick={() => handleButtonClick(index)}
-            />
+          <button
+          key={index}
+          className={`push2-button ${state ? 'on' : 'off'}`}
+          onClick={() => handleButtonClick(index)}
+          ></button>
         ))}
         </div>
     </div>
